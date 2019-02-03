f3d25ad4583610d3ead34da1e9e1d8a4b3382bf6
motiwu <moti.yuchuanwu@gmail.com>
2019-02-03 12:34:44 +0800

    mark animation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+function toggleMarks(type) {
+    let marks = [
+        document.getElementById('marks-heart'),
+        document.getElementById('marks-nite'),
+        // document.getElementById('marks-expo'),
+        document.getElementById('marks-info'),
+    ];
+
+    marks.forEach(mark => {
+        mark.classList.remove('active');
+    });
+    document.getElementById('marks-' + type).classList.add('active');
+}
+
+
+
 function openDialog() {
     let dialog = document.getElementById('dialog-route');
     dialog.classList.add('active');
