a83fba9cc6e8834a9599bb75a54ed1036ceb866a
motiwu <moti.yuchuanwu@gmail.com>
2019-02-17 00:18:49 +0800

    spots

--- js/main.js
+++ js/main.js
@@ -7,3 +7,25 @@ function cloasDialog() {
     let dialog = document.getElementById('dialog-route');
     dialog.classList.remove('active');
 }
+
+
+
+let styles = getComputedStyle(document.documentElement);
+let totalSpot = document.querySelectorAll('.slider-wrapper .slider li').length;
+console.log(totalSpot);
+
+function gospot(target) {  
+    document.documentElement.style.setProperty('--currentSpot', target);
+}
+
+function nextspot() {
+    let currentSpot = styles.getPropertyValue('--currentSpot');
+    if (currentSpot < totalSpot - 1)
+        document.documentElement.style.setProperty('--currentSpot', Number(currentSpot) + 1);
+}
+
+function prevspot() {
+    let currentSpot = styles.getPropertyValue('--currentSpot');
+    if (currentSpot > 0)
+        document.documentElement.style.setProperty('--currentSpot', Number(currentSpot) - 1);
+}
